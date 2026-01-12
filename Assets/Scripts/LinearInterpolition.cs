7450dea1f8d6bcc43b6b6c7e4910a78cb4e6e4ee
Adam Garzon <garzonad@shernet.sheridancollege.ca>
2026-01-12 10:13:28 -0500

    Added an animation curve to change the size of something and eased the lerp

--- Assets/Scripts/LinearInterpolition.cs
+++ Assets/Scripts/LinearInterpolition.cs
@@ -5,6 +5,8 @@ public class LinearInterpolition : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t = 0;
+
+    public AnimationCurve curve;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +22,7 @@ public class LinearInterpolition : MonoBehaviour
             t = 0; 
         }
 
-        transform.position = Vector2.Lerp(start.position, end.position, t);
+        //transform.position = Vector2.Lerp(start.position, end.position, t);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
     }
 }
